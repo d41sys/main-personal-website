af3be29e63cb8ad23ce7735b6d9ac540b00e5bf7
adatdat <1712328@student.hcmus.edu.vn>
2023-05-15 23:21:35 +0900

    rebuild website

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+'use client'
+
+import Image from 'next/image'
+import { Card } from '@nextui-org/react';
+
+
+export default function HomePage() {
+  return (
+    <Card>
+      dss
+    </Card>
+  )
+}

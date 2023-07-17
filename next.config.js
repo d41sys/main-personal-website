52637281d5ce4609286e8d7981f9265e7975b7a5
adatdat <1712328@student.hcmus.edu.vn>
2023-07-17 17:05:12 +0900

    fix deploy5

--- next.config.js
+++ next.config.js
@@ -13,46 +13,7 @@ const nextConfig = {
   },
   images : {
     domains: ["flowbite.s3.amazonaws.com", "cdn.dribbble.com", "images.pexels.com"]
-  },
-  // redirects() {
-  //   return [
-  //     {
-  //       source: "/components",
-  //       destination: "/docs/components/accordion",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/components",
-  //       destination: "/docs/components/accordion",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/docs/components",
-  //       destination: "/docs/components/accordion",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/examples",
-  //       destination: "/examples/dashboard",
-  //       permanent: false,
-  //     },
-  //     {
-  //       source: "/docs/primitives/:path*",
-  //       destination: "/docs/components/:path*",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/figma",
-  //       destination: "/docs/figma",
-  //       permanent: true,
-  //     },
-  //     {
-  //       source: "/docs/forms",
-  //       destination: "/docs/forms/react-hook-form",
-  //       permanent: false,
-  //     },
-  //   ]
-  // },
+  }
 }
 
 module.exports = withContentlayer(nextConfig)
